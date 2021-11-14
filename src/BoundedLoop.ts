1bc567986a42def5aea9b081110b88a9710cf0d0
Jeremy Neander <jeremy.neander@gmail.com>
2021-11-14 10:13:41 -0600

    chore: convert to typescript

--- src/BoundedLoop.ts
+++ src/BoundedLoop.ts
@@ -1,34 +1,42 @@
-function timeForAnotherLoop(startTime, iterations) {
+function timeForAnotherLoop(startTime: number, iterations: number): boolean {
   const difference = Math.floor(Date.now()) - startTime
   const average = difference / iterations
   return difference + average < 15
 }
 
+type BoundedLoopConfig = {
+  loopFn(): void
+}
+
 export default class BoundedLoop {
-  constructor(config = {}) {
-    this.config = {...config}
+  private loopFn: BoundedLoopConfig['loopFn']
+  private interval: number | null
+
+  constructor({loopFn}: BoundedLoopConfig) {
+    this.loopFn = loopFn
+
     this.interval = null
   }
 
-  start() {
+  start(): void {
     if (this.interval == null) {
       const boundedLoop = () => {
         const startTime = Math.floor(Date.now())
         let iterations = 1
 
         while (this.interval != null && timeForAnotherLoop(startTime, iterations)) {
-          this.config.loopFn()
+          this.loopFn()
           iterations++
         }
       }
 
       this.interval = setInterval(() => {
-        boundedLoop(this.config.loopFn)
+        boundedLoop()
       }, 0)
     }
   }
 
-  stop() {
+  stop(): void {
     if (this.interval != null) {
       clearInterval(this.interval)
       this.interval = null
